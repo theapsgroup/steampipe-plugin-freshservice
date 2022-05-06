af789d2ca9f39ff91ba671ee39d9a8ba46fec1fe
Graza <code@graza.io>
2022-05-06 14:21:52 +0100

    feat: sla table

---
+++ docs/tables/freshservice_sla_policy.md
@@ -0,0 +1,14 @@
+# Table: freshservice_sla_policy
+
+Obtain information on Service Level Agreement Policies defined in the FreshService instance.
+
+## Examples
+
+### List all SLA Policies
+
+```sql
+select
+  *
+from
+  freshservice_sla_policy;
+```
--- freshservice/plugin.go
+++ freshservice/plugin.go
@@ -39,6 +39,7 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 			"freshservice_release_task":        tableReleaseTask(),
 			"freshservice_release_timeentry":   tableReleaseTimeEntry(),
 			"freshservice_service":             tableService(),
+			"freshservice_sla_policy":          tableSlaPolicy(),
 			"freshservice_ticket":              tableTicket(),
 			"freshservice_ticket_conversation": tableTicketConversation(),
 			"freshservice_ticket_task":         tableTicketTask(),
