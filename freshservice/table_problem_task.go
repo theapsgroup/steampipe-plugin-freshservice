26e1ce57c573f6150b16181912fc8ad92d56caa5
Graza <code@graza.io>
2022-05-06 11:33:44 +0100

    chore: updated deps

--- freshservice/table_problem_task.go
+++ freshservice/table_problem_task.go
@@ -4,9 +4,9 @@ import (
 	"context"
 	"fmt"
 	fs "github.com/theapsgroup/go-freshservice/freshservice"
-	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
-	"github.com/turbot/steampipe-plugin-sdk/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/plugin/transform"
+	"github.com/turbot/steampipe-plugin-sdk/v3/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v3/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v3/plugin/transform"
 )
 
 func tableProblemTask() *plugin.Table {
