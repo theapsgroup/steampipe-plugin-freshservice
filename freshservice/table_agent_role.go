40e7e4a1726634d308e2c4a1b1674d775d8035c8
Graza <code@graza.io>
2022-10-07 18:41:35 +0100

    feat: upgraded to go1.19/sdk4.1.7

--- freshservice/table_agent_role.go
+++ freshservice/table_agent_role.go
@@ -4,8 +4,8 @@ import (
 	"context"
 	"fmt"
 	fs "github.com/theapsgroup/go-freshservice/freshservice"
-	"github.com/turbot/steampipe-plugin-sdk/v3/grpc/proto"
-	"github.com/turbot/steampipe-plugin-sdk/v3/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v4/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v4/plugin"
 )
 
 func tableAgentRole() *plugin.Table {
